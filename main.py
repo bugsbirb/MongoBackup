4296e7b9591ef0cdf538641e57b61fe25d541b14
Bugs <deezbird2768@gmail.com>
2024-07-20 21:30:33 +0100

    ea

--- main.py
+++ main.py
@@ -9,38 +9,37 @@ import dropbox
 
 load_dotenv()
 mongoUri = os.getenv('mongo_uri')
-dbname = os.getenv('db_name')
+dbnames = os.getenv('db_names').split(',')
 dropbox_key = os.getenv('dropbox_key')
 interval = int(os.getenv('interval'))
 
-
-
 def run_backup():
     client = MongoClient(mongoUri)
-    db = client[dbname]
-    collections = db.list_collection_names()
-    files_to_compress = []
-    directory = create_folder_backup(dbname)
-    
-    for collection in collections:
-        db_collection = db[collection]
-        cursor = db_collection.find({})
-        filename = f'{directory}/{collection}.json'
-        files_to_compress.append(filename)
+    for dbname in dbnames:
+        db = client[dbname]
+        collections = db.list_collection_names()
+        files_to_compress = []
+        directory = create_folder_backup(dbname)
         
-        with open(filename, 'w') as file:
-            file.write('[')
-            first = True
-            for document in cursor:
-                if not first:
-                    file.write(',')
-                file.write(dumps(document))
-                first = False
-            file.write(']')
-    
-    tar_file = f'{directory}.tar.gz'
-    make_tarfile(tar_file, files_to_compress)
-    upload_to_dropbox(tar_file)
+        for collection in collections:
+            db_collection = db[collection]
+            cursor = db_collection.find({})
+            filename = f'{directory}/{collection}.json'
+            files_to_compress.append(filename)
+            
+            with open(filename, 'w') as file:
+                file.write('[')
+                first = True
+                for document in cursor:
+                    if not first:
+                        file.write(',')
+                    file.write(dumps(document))
+                    first = False
+                file.write(']')
+        
+        tar_file = f'{directory}.tar.gz'
+        make_tarfile(tar_file, files_to_compress)
+        upload_to_dropbox(tar_file)
 
 def make_tarfile(output_filename, files_to_compress):
     with tarfile.open(output_filename, "w:gz") as tar:
